fc43f30a8b2ba3f372d5fb67a68402d29cc0aca9
wyg <m15616010182@163.com>
2021-05-08 20:42:00 +0800

    w

--- src/views/measures/index.vue
+++ src/views/measures/index.vue
@@ -1,40 +1,66 @@
 <template>
   <div :class="prefixCls">
-    <div class="select-panel w">
-      城市政策查询:
-      <el-select v-model="value" placeholder="请选择">
-        <el-option
-          v-for="item in options"
-          :key="item.value"
-          :label="item.label"
-          :value="item.value"
-        >
-        </el-option>
-      </el-select>
-    </div>
+    <img src="../../assets/images/set_out.png" alt="" />
+    <div class="select-panel w"></div>
     <div class="content w">
-      <el-card class="box-card">
-        <div slot="header" class="clearfix">
-          <span>卡片名称</span>
-          <el-button style="float: right; padding: 3px 0" type="text"
-            >操作按钮</el-button
-          >
-        </div>
-        <div v-for="o in 4" :key="o" class="text item">
-          {{ "列表内容 " + o }}
-        </div>
-      </el-card>
-      <el-card class="box-card">
-        <div slot="header" class="clearfix">
-          <span>卡片名称</span>
-          <el-button style="float: right; padding: 3px 0" type="text"
-            >操作按钮</el-button
-          >
-        </div>
-        <div v-for="o in 4" :key="o" class="text item">
-          {{ "列表内容 " + o }}
-        </div>
-      </el-card>
+      <div>
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <span class="city-name">长沙</span>
+            <el-button style="float: right; padding: 3px 0" type="text"
+              >低风险地区</el-button
+            >
+          </div>
+          <div class="text item">
+            出城政策
+            <p>
+              请密切关注近期国内本土疫情动态，合理安排出行。尽量减少不必要的出行，如确需出行，请提前关注旅游目的地疫情防控规定，关注交通、天气信息，错峰出行。遵守景区疫情防护要求、安全警示和文明旅游规定，自觉配合体温检测、查验健康码等防疫措施，保持戴口罩、勤洗手、用公筷等良好卫生习惯。保持安全社交距离，健康文明出行。如非必要，请不要前往云南瑞丽旅行及出差。确需前往的，请务必提前向所在社区(村)和工作单位报备，并做好个人防护，配合疫情防控部门做好核酸检测、健康监测等疫情防控措施。
+            </p>
+          </div>
+        </el-card>
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <span class="city-name">株洲</span>
+            <el-button style="float: right; padding: 3px 0" type="text"
+              >低风险地区</el-button
+            >
+          </div>
+          <div class="text item">
+            出城政策
+            <p>
+              在恢复日常生活、增加出行的同时，勤洗手、戴口罩等日常防护依旧不能少。
+            </p>
+          </div>
+        </el-card>
+      </div>
+      <div>
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <span class="city-name">湘潭</span>
+            <el-button style="float: right; padding: 3px 0" type="text"
+              >低风险地区</el-button
+            >
+          </div>
+          <div class="text item">
+            出城政策
+            <p>当地暂无最新官方政策发布，若有，会第一时间更新。</p>
+          </div>
+        </el-card>
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <span class="city-name">湖北</span>
+            <el-button style="float: right; padding: 3px 0" type="text"
+              >低风险地区</el-button
+            >
+          </div>
+          <div class="text item">
+            出城政策
+            <p>
+              在恢复日常生活、增加出行的同时，勤洗手、戴口罩等日常防护依旧不能少。
+            </p>
+          </div>
+        </el-card>
+      </div>
     </div>
   </div>
 </template>
@@ -90,11 +116,21 @@ $prefixCls: "views-measures";
     }
   }
   .content {
-    display: flex;
+    > div {
+      display: flex;
+    }
+
     .el-card {
+      margin-bottom: 20px;
       flex: 1;
       &:last-child {
         margin-left: 20px;
+      }
+      .city-name {
+        padding-left: 15px;
+        font-size: 18px;
+        font-weight: 700;
+        border-left: 4px solid $--color-primary;
       }
     }
   }
