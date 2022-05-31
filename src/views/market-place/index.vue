3d72ee6dd1b903c11b26efa862b62a7890340bb1
wangpeng <1137106486@qq.com>
2022-05-31 14:22:38 +0800

    w

--- src/views/market-place/index.vue
+++ src/views/market-place/index.vue
@@ -167,7 +167,7 @@
                 </div>
                 <el-pagination background layout="total, prev, pager, next"
                     @current-change="onPageChange" @prev-click="onPageChange" @next-click="onPageChange"
-                    :total="Number(total)"
+                    :total="Number(total)" :hide-on-single-page="true" 
                     :current-page="Number(page.curPage)" v-if="list.length && !isLoading">
                 </el-pagination>
             </div>
